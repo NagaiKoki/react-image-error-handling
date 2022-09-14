0ebee3db924ca508f28aa886164bc16171759d6c
koki <kokinagai200@gmail.com>
2022-09-14 17:30:06 +0900

    fix

--- examples/web/pages/index.tsx
+++ examples/web/pages/index.tsx
@@ -1,10 +1,19 @@
 import { useImageErrorHandling } from "react-image-error-handling";
 
+const errorImageUrl =
+  "https://error.avatars.githubusercontent.com/u/50698194?v=4";
+const myGithubUrl = "https://avatars.githubusercontent.com/u/50698194?v=4";
+
 export default function Web() {
-  useImageErrorHandling();
+  const { ref, loaded } = useImageErrorHandling({ imageUrl: errorImageUrl });
+
+  console.log(loaded);
   return (
     <div>
       <h1>Web</h1>
+      <div style={{ width: 400, height: 400 }}>
+        <img ref={ref} src={myGithubUrl} alt="react-error-handling-image" />
+      </div>
     </div>
   );
 }
--- packages/react-image-error-handling/src/useImageErrorHandling.ts
+++ packages/react-image-error-handling/src/useImageErrorHandling.ts
@@ -1,7 +1,65 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect, useRef, RefObject } from "react";
 
-export const useImageErrorHandling = () => {
+type ArgsType = {
+  imageUrl: string;
+  maxRetryCount?: number;
+  retryInterval?: number;
+};
+
+type ReturnType = {
+  ref: RefObject<HTMLImageElement>;
+  loaded: boolean;
+  onLoaded: () => void;
+  onError: () => void;
+};
+
+export const useImageErrorHandling = ({
+  imageUrl,
+  maxRetryCount,
+  retryInterval,
+}: ArgsType): ReturnType => {
+  const [errorCount, setErrorCount] = useState(0);
   const [loaded, setLoaded] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
 
-  console.log("hoge");
+  const handleOnLoaded = useCallback(() => {
+    if (imageRef.current) {
+      setLoaded(true);
+    }
+  }, [imageRef]);
+
+  const handleOnError = useCallback(() => {
+    if (imageRef.current)
+      [
+        (imageRef.current.onerror = () => {
+          setErrorCount(errorCount + 1);
+        }),
+      ];
+  }, [errorCount, imageRef]);
+
+  // const handleOnRetry = useCallback(() => {
+  //   if (imageRef.current) {
+  //     if (errorCount < maxRetryCount) {
+  //       imageRef.current.src = imageUrl;
+  //     }
+  //   }
+  // }, [errorCount, maxRetryCount, imageUrl]);
+
+  useEffect(() => {
+    if (imageRef.current) {
+      const currentImage = imageRef.current;
+      currentImage.addEventListener("load", handleOnLoaded);
+
+      return () => {
+        currentImage.removeEventListener("load", handleOnLoaded);
+      };
+    }
+  }, [handleOnLoaded, imageRef]);
+
+  return {
+    ref: imageRef,
+    loaded,
+    onLoaded: handleOnLoaded,
+    onError: handleOnError,
+  };
 };
